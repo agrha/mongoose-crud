69827c52fb336a8036b56b5e176179aa06dec02b
agrha <agrhakartz@gmail.com>
2018-03-06 21:44:01 +0700

    tambah update transaction

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -29,6 +29,7 @@ class Transactions {
       member:req.body.customerId,
       days:req.body.days,
       out_date: new Date(),
+      status:'belum dikembalikan'
       due_date: moment().add(6,'days'),
       booklist:[req.body.bookId1,req.body.bookId2]
     }
@@ -46,21 +47,27 @@ class Transactions {
     transactionsSchema.findOne({_id:req.body.id})
     .exec()
     .then(data=>{
-      let in_date = moment().add(12,'days')
-
-      let fine_days
+      let in_date = req.body.in_date
+      let fine_days = in_date.getDate()-data.due_date.getDate()
       // console.log(data);
       let objUpdate = {
         in_date: in_date,
-        fine: data.booklist.length * 1000
+        fine: data.booklist.length * 1000 * fine_days,
+        status:'dikembalikan'
       }
-
-      // transactionsSchema.findOneAndUpdate(data,)
+      transactionsSchema.findOneAndUpdate(data,objUpdate)
+      .then(()=>{
+        res.status(200).json({
+          message:'buku sudah dikembalikan '
+        })
+      }).catch(err=>{
+        message:'gagal dalam proses update'
+      })
     })
   }
 
   static deleteTransactions(req,res){
-    transactionsSchema.remove()
+    transactionsSchema.remove({_id:req.params._id})
     .exec()
     .then(()=>{
       res.status(200).json({
